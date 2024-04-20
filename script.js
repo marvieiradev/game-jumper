d6e3312389fbb200d0d9578deb0f7474bd8cdaf9
Marcos Vieira <marcosvieira482@gmail.com>
2024-04-19 23:32:13 -0300

    feat: update and fix javascript codes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const player = document.querySelector('.player');
-const pipe = document.querySelector('.pipe');
+const spikes = document.querySelector('.spikes');
+const clouds = document.querySelector('.clouds');
 
 const jump = () => {
     player.classList.add('jump');
@@ -9,15 +10,23 @@ const jump = () => {
 }
 
 const gameLoop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft;
+    const spikesPosition = spikes.offsetLeft;
+    const cloudsPosition = clouds.offsetLeft;
     const playerPosition = +window.getComputedStyle(player).bottom.replace('px', '');
 
-    if (pipePosition <= 120 && pipePosition > 0 && playerPosition < 80) {
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
+    if (spikesPosition <= 110 && spikesPosition > 0 && playerPosition < 80) {
+        spikes.style.animation = 'none';
+        spikes.style.left = `${spikesPosition}px`;
 
         player.style.animation = 'none';
         player.style.bottom = `${playerPosition}px`;
+
+        player.src = './images/game-over.png';
+
+        clouds.style.left = `${cloudsPosition}px`;
+        clouds.style.animation = 'none';
+
+        clearInterval(gameLoop);
     }
 }, 10)
 
