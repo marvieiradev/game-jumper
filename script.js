884c5b3dcab1ebb074e46380ccd21ce4c1d0fa21
Marcos Vieira <marcosvieira482@gmail.com>
2024-04-23 23:39:28 -0300

    feat: add score and hi-score implementation

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const player = document.querySelector('.player');
 const spikes = document.querySelector('.spikes');
 const clouds = document.querySelector('.clouds');
 const gameOver = document.querySelector('.game-over');
+const scoreTxt = document.querySelector('.score');
+const hiScoreTxt = document.querySelector('.hi-score');
 const btRestart = document.getElementById('bt-restart');
-
+var points = 0;
+const hiScore = Number(localStorage.getItem("hi-score")) || 0;
 const jump = () => {
     player.classList.add('jump');
     setTimeout(() => {
@@ -15,7 +18,8 @@ const gameLoop = setInterval(() => {
     const spikesPosition = spikes.offsetLeft;
     const cloudsPosition = clouds.offsetLeft;
     const playerPosition = +window.getComputedStyle(player).bottom.replace('px', '');
-
+    points += 1;
+    var score = Math.round(points / 100)
     if (spikesPosition <= 110 && spikesPosition > 0 && playerPosition < 80) {
         spikes.style.animation = 'none';
         spikes.style.left = `${spikesPosition}px`;
@@ -30,8 +34,15 @@ const gameLoop = setInterval(() => {
 
         gameOver.style.display = 'flex';
 
+        if (score >= hiScore) {
+            hiScore = score
+        }
+        localStorage.setItem("hi-score", hiScore);
+
         clearInterval(gameLoop);
     }
+    scoreTxt.innerText = score.toString().padStart(5, '0');
+    hiScoreTxt.innerText = "HI " + hiScore.toString().padStart(5, '0');
 }, 10);
 
 btRestart.addEventListener('click', () => {
